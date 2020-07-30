ef3710297236d36059a32559a662c48f3e614a9d
daileytj <daileytj89@gmail.com>
2020-07-30 12:41:56 -0400

    Add menu button to landing page

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,26 @@
 import React from 'react';
-import { Header } from '@pxblue/react-native-components';
+import { Header, wrapIcon } from '@pxblue/react-native-components';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-export const Home: React.FC = () => <Header title={'Design Pattern Examples'} />;
+const MenuIcon = wrapIcon({ IconClass: MaterialIcons, name: 'menu' });
+
+export const Home: React.FC = () => {
+    const navigation = useNavigation();
+
+    const toggleMenu = (): void => {
+        navigation.openDrawer();
+    };
+
+    return (
+        <Header
+            title={'Design Pattern Examples'}
+            navigation={{
+                icon: MenuIcon,
+                onPress: (): void => {
+                    toggleMenu();
+                },
+            }}
+        />
+    );
+};
