7483132a339598fdcaa94bbb9b9d26a2ac1e959d
daileytj <daileytj89@gmail.com>
2020-08-05 10:19:18 -0400

    Add menu button to collapsible appbar example

--- examples/collapsible-appbar/CollapsibleAppbar.tsx
+++ examples/collapsible-appbar/CollapsibleAppbar.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Header } from '@pxblue/react-native-components';
+import { Header, wrapIcon } from '@pxblue/react-native-components';
 import bg from '../../assets/images/farm.jpg';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-export const CollapsibleAppbarScreen: React.FC = () => (
-    <Header
-        expandable
-        startExpanded
-        backgroundImage={bg}
-        title={'Collapsible Appbar'}
-        subtitle={'Tap to collapse/expand'}
-        info={`I'm hidden when collapsed`}
-    />
-);
+const MenuIcon = wrapIcon({ IconClass: MaterialIcons, name: 'menu' });
+
+export const CollapsibleAppbarScreen: React.FC = () => {
+    const navigation = useNavigation();
+
+    const toggleMenu = (): void => {
+        navigation.openDrawer();
+    };
+
+    return (
+        <Header
+            expandable
+            startExpanded
+            backgroundImage={bg}
+            title={'Collapsible Appbar'}
+            subtitle={'Tap to collapse/expand'}
+            info={`I'm hidden when collapsed`}
+            navigation={{
+                icon: MenuIcon,
+                onPress: (): void => {
+                    toggleMenu();
+                },
+            }}
+        />
+    );
+};
