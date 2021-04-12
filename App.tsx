5fcea82ce606ba242bbe1421e2f8240d23f2b8f9
joebochill <boyle.p.joseph@gmail.com>
2021-04-12 08:52:19 -0400

    Fix up the config to work with latest expo

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import * as PXBThemes from '@pxblue/react-native-themes';
 import AppLoading from 'expo-app-loading';
 import { MyDrawer } from './router';
 import { NavigationContainer } from '@react-navigation/native';
+import { H6 } from '@pxblue/react-native-components';
 
 const App: React.FC = () => {
     const [fontsLoaded] = useFonts({
